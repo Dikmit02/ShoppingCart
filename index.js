184da38ae2deaa72f32405117fffc3ab40137f5d
Dikmit02 <dikshamittal21297@gmail.com>
2020-07-05 12:52:10 +0530

    setUp

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
-
+const path=require('path')
+const hbs=require('hbs')
+const expressHbs = require('express-handlebars');
 require('./db/mongoose')
+
+const routes=require('./routes/index')
 
 const app = express()
 
 const port = process.env.PORT 
 
+const publicDirectoryPath = path.join(__dirname, '/public')
+const viewsPath = path.join(__dirname, 'views')
 
+//  use when index.html is a static page 
+app.use(express.static(path.join(publicDirectoryPath)))
+
+// //Setup handlebars engine and view location
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+// app.set('views', viewsPath)
+// hbs.registerPartials(partialPath)
+
+app.use('/',routes)
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
